7cdbbb5c45487ee63dd5468d59488c60207017e8
Tridib Jyoti Das <tridibjdas@gmail.com>
2024-08-27 22:36:18 +0530

    Updated test script for mayo 2016 training

--- train_mayo2016.sh
+++ train_mayo2016.sh
@@ -1,12 +1,12 @@
-CUDA_VISIBLE_DEVICES=gpu_id python main.py\
+python main.py\
  --model_name corediff\
- --run_name dose25_mayo_2016\
+ --run_name dose25_mayo2016\
  --batch_size 4\
  --max_iter 150000\
+ --train_dataset mayo_2016\
  --test_dataset mayo_2016\
- --test_id 9\
+ --test_id 8 9\
  --context\
  --only_adjust_two_step\
  --dose 25\
- --save_freq 2500\
- --train_dataset mayo_2016\
+ --save_freq 2500
